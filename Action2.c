93685b63bce0a54794fc086dd699ce4c4473448d
CyrilZxy <54348995+CyrilZxy@users.noreply.github.com>
2020-07-12 23:26:36 +0800

    性能测试 函数

---
+++ Action2.c
@@ -0,0 +1,68 @@
+Action()
+{
+
+	web_url("WebTours", 
+		"URL=http://127.0.0.1:1080/WebTours", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_think_time(5);
+
+	lr_start_transaction("login_transaction");
+
+	lr_rendezvous("login_zvous");
+
+	web_reg_find("Search=Body",
+		"Text=Welcome, <b>{username}</b>, to the Web Tours reservation pages.","SaveCount=reservation_Count",
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='{username}' Name ='CorrelationParameter' Type ='Manual'*/
+	web_reg_save_param_ex(
+		"ParamName=CorrelationParameter",
+		"LB=blockquote>Welcome, <b>",
+		"RB=<",
+		SEARCH_FILTERS,
+		"Scope=Body",
+		"RequestUrl=*/login.pl*",
+		"NotFound=warning" ,
+		LAST);
+
+	web_submit_form("login.pl", 
+		"Snapshot=t2.inf", 
+		ITEMDATA, 
+		"Name=usern" 
+		"ame", "Value={username}", ENDITEM,
+		"Name=password", "Value=12" 
+		"3", ENDITEM,
+		"Name=login.x", "Value=76", ENDITEM, 
+		"Name=login.y", "Value=8", ENDITEM, 
+		LAST);
+
+ 
+ 
+if( strcmp( lr_eval_string ("{username}"),"zxy" ) ==0 ){
+	lr_log_message("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	lr_output_message("username=%s",lr_eval_string("{username}"));
+	lr_log_message("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+}else{
+	lr_log_message("this user is not zxy!!!!!!!!!!");
+}
+
+
+	
+	lr_end_transaction("login_transaction",LR_AUTO);
+
+	lr_think_time(5);
+
+
+	web_image("SignOff Button", 
+		"Alt=SignOff Button", 
+		"Snapshot=t3.inf", 
+		LAST);
+
+	return 0;
+}
